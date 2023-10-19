c8740ee77d5e4cdf28be7633ce2a01ecfd4b7f44
Dawid Nowak <nowakd@gmail.com>
2023-10-19 20:06:15 +0100

    Getting serious

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 
 ```cross build --target armv7-unknown-linux-gnueabihf```  
 ```scp target/armv7-unknown-linux-gnueabihf/debug/all_in_one_enviro_mini dawid@192.168.1.27://home/dawid/Workspace```
+
+
+
+
+```
+/usr/bin/docker build --label 'org.cross-rs.for-cross-target=armv7-unknown-linux-gnueabihf' --label 'org.cross-rs.workspace_root=/home/dawid/Workspace/pi/enviroplus-rust/all_in_one_enviro_mini' --tag cross-custom-all_in_one_enviro_mini:armv7-unknown-linux-gnueabihf-92fc0-pre-build --build-arg 'CROSS_CMD=dpkg --add-architecture armv7 && apt-get update && apt-get install --assume-yes wget' --build-arg 'CROSS_DEB_ARCH=armhf' --file /home/dawid/Workspace/pi/enviroplus-rust/all_in_one_enviro_mini/target/armv7-unknown-linux-gnueabihf/Dockerfile.armv7-unknown-linux-gnueabihf-custom /home/dawid/Workspace/pi/enviroplus-rust/all_in_one_enviro_mini
+
+
+
+/usr/bin/docker run -e 'RUST_BACKTRACE=1' -e 'PKG_CONFIG_ALLOW_CROSS=1' -e 'XARGO_HOME=/xargo' -e 'CARGO_HOME=/cargo' -e 'CARGO_TARGET_DIR=/target' -e 'CROSS_RUNNER=' -e TERM -e 'USER=dawid' -v /home/dawid/Workspace/pi/enviroplus-rust/embedded-canvas:/home/dawid/Workspace/pi/enviroplus-rust/embedded-canvas -v /home/dawid/Workspace/pi/enviroplus-rust/ltr-559:/home/dawid/Workspace/pi/enviroplus-rust/ltr-559 -v /home/dawid/Workspace/pi/enviroplus-rust/rust-portaudio:/home/dawid/Workspace/pi/enviroplus-rust/rust-portaudio -v 
+/home/dawid/Workspace/pi/enviroplus-rust/rust-portaudio/rust-portaudio-sys:/home/dawid/Workspace/pi/enviroplus-rust/rust-portaudio/rust-portaudio-sys -v /home/dawid/Workspace/pi/enviroplus-rust/st7735-lcd-rs:/home/dawid/Workspace/pi/enviroplus-rust/st7735-lcd-rs -v /home/dawid/.xargo:/xargo:z -v /home/dawid/.cargo:/cargo:z -v /cargo/bin -v /home/dawid/Workspace/pi/enviroplus-rust/all_in_one_enviro_mini:/home/dawid/Workspace/pi/enviroplus-rust/all_in_one_enviro_mini:z -v /home/dawid/.rustup/toolchains/stable-x86_64-unknown-linux-gnu:/rust:z,ro -v /home/dawid/Workspace/pi/enviroplus-rust/all_in_one_enviro_mini/target:/target:z -w /home/dawid/Workspace/pi/enviroplus-rust/all_in_one_enviro_mini -i -t cross-custom-all_in_one_enviro_mini:armv7-unknown-linux-gnueabihf-92fc0-pre-build sh -c 'PATH=$PATH:/rust/bin cargo -v build --target armv7-unknown-linux-gnueabihf'
+
+
+
+
+```
+
+
+## Crossbuilding
+
+```shell
+apt-get install gcc-arm-linux-gnueabihf make autoconf automake git
+export CC=arm-linux-gnueabihf-gcc
+export AR=/usr/bin/arm-linux-gnueabihf-gcc-ar
+ln -s /usr/bin/arm-linux-gnueabihf-gcc-ar /usr/bin/ar
+ln -s /usr/arm-linux-gnueabihf/bin/objdump /usr/bin/objdump
+git clone https://github.com/PortAudio/portaudio.git
+./configure --disable-shared --enable-static  --with-pic --host=arm-linux-gnueabihf
+
+rustup toolchain install stable-arm-unknown-linux-gnueabihf
+rustup target add arm-unknown-linux-gnueabihf
+apt install build-essential wget
+git clone https://github.com/RustAudio/rust-portaudio.git
+
+
+./configure --disable-shared --enable-static --with-alsa --with-pic --host arm-linux-gnueabihf
+export LIBS='-lpthread -ldl'
+
+	    //	    .args(&["--enable-shared"]) // Only build static lib
+	    .env("LIBS","-lpthread -ldl")
+	    .args(&["--host", "arm-linux-gnueabihf"]) // cross compile
+//	    .args(&["--with-alsa"]) // Build position-independent code (required by Rust)
+```
--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,20 @@ use std::error::Error;
 use systemstat::{Platform, System};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+
+
+use portaudio as pa;
+
+
+const INTERLEAVED: bool = true;
+const LATENCY: pa::Time = 0.0; // Ignored by PortAudio::is_*_format_supported.
+const STANDARD_SAMPLE_RATES: [f64; 13] = [
+    8000.0, 9600.0, 11025.0, 12000.0, 16000.0, 22050.0, 24000.0, 32000.0, 44100.0, 48000.0,
+    88200.0, 96000.0, 192000.0,
+];
+
+
+
 struct NotConnected;
 
 impl OutputPin for NotConnected {
@@ -61,6 +75,71 @@ impl OutputPin for NotConnected {
 const SPI_SPEED: u32 = 10_000_000;
 const DISPLAY_SIZE: Size = Size::new(160, 80);
 
+fn check_audio()-> Result<(), Box<dyn Error>>{
+    let pa = pa::PortAudio::new()?;
+    tracing::info!("PortAudio version: {} {}", pa.version(), pa.version_text()?);
+    let host_count = pa.host_api_count()?;
+    tracing::info!("PortAudio host count {host_count}");
+    let default_host = pa.default_host_api();
+    let default_input = pa.default_input_device();
+//    let input_info = pa.device_info(default_input);
+    
+    tracing::info!("PortAudio host count {host_count:?} {default_host:?} {default_input:?}");
+    //    tracing::info!("PortAudio default input device info: {:#?}", &input_info);
+    let devices = pa.device_count()?;
+    tracing::info!("Devices {devices}");
+    for device in pa.devices()? {
+        let (idx, info) = device?;
+        tracing::info!("--------------------------------------- {:?}", idx);
+        tracing::info!("{:#?}", &info);
+
+        let in_channels = info.max_input_channels;
+        let input_params = pa::StreamParameters::<i16>::new(idx, in_channels, INTERLEAVED, LATENCY);
+        let out_channels = info.max_output_channels;
+        let output_params =
+            pa::StreamParameters::<i16>::new(idx, out_channels, INTERLEAVED, LATENCY);
+
+        tracing::info!(
+            "Supported standard sample rates for half-duplex 16-bit {} channel input:",
+            in_channels
+        );
+        for &sample_rate in &STANDARD_SAMPLE_RATES {
+            if pa
+                .is_input_format_supported(input_params, sample_rate)
+                .is_ok()
+            {
+                tracing::info!("\t{}hz", sample_rate);
+            }
+        }
+
+        tracing::info!(
+            "Supported standard sample rates for half-duplex 16-bit {} channel output:",
+            out_channels
+        );
+        for &sample_rate in &STANDARD_SAMPLE_RATES {
+            if pa
+                .is_output_format_supported(output_params, sample_rate)
+                .is_ok()
+            {
+                tracing::info!("\t{}hz", sample_rate);
+            }
+        }
+
+        tracing::info!("Supported standard sample rates for full-duplex 16-bit {} channel input, {} channel output:",
+                 in_channels, out_channels);
+        for &sample_rate in &STANDARD_SAMPLE_RATES {
+            if pa
+                .is_duplex_format_supported(input_params, output_params, sample_rate)
+                .is_ok()
+            {
+                tracing::info!("\t{}hz", sample_rate);
+            }
+        }
+    }
+    
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
@@ -70,81 +149,22 @@ fn main() -> Result<(), Box<dyn Error>> {
         .init();
 
     tracing::info!("Hello PI");
+    check_audio()?;
 
-    let sys = System::new();
-
-    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss1, SPI_SPEED, SPIMode::Mode0)?;
-    let gpio = Gpio::new()?;
-    let dc = gpio.get(9)?.into_output();
-    let mut proximity = gpio.get(4)?.into_input_pullup();
-    proximity.set_async_interrupt(Trigger::FallingEdge, |c: Level| {
-        tracing::warn!("Got interrupt level {c:?}");
-    })?;
+    let sys = System::new();    
+    let gpio = Gpio::new()?;    
     let mut backlight = gpio.get(12)?.into_output();
 
     backlight.set_low();
     thread::sleep(Duration::from_millis(100));
     backlight.set_high();
     thread::sleep(Duration::from_millis(100));
-
-    let i2c = I2c::new()?;
-    let mut bme280 = BME280::new_primary(i2c, Delay);
-    bme280.init().unwrap();
-
-    let i2c = I2c::new()?;
-    let mut light_sensor = Ltr559::new_device(i2c, SlaveAddr::default());
-    light_sensor.set_als_contr(AlsGain::Gain4x, true, false).unwrap();
-    thread::sleep(Duration::from_millis(100));
-    light_sensor.reset_internal_driver_state();
-    thread::sleep(Duration::from_millis(100));
     
-
-    let manufacturer_id = light_sensor.get_manufacturer_id().unwrap();
-    let part_id = light_sensor.get_part_id().unwrap();
+    let mut bme280 = init_pth_sensors(I2c::new()?)?;
+    let mut light_sensor = init_light_sensor(I2c::new()?)?;        
+    let mut display = init_display(gpio)?;
     
-    let als_status = light_sensor.get_als_contr().unwrap();
-    let ps_status = light_sensor.get_ps_contr().unwrap();
-    tracing::info!("Manufactured by {manufacturer_id:x} : {part_id:x} status {als_status:x} {ps_status:x}" );        
     
-    light_sensor
-        .set_interrupt(InterruptPinPolarity::Low, InterruptMode::OnlyPS)
-        .unwrap();
-            
-    light_sensor.set_ps_led(LedPulse::Pulse30, LedDutyCycle::_100, LedCurrent::_50mA).unwrap();
-    light_sensor.set_ps_n_pulses(1).unwrap();
-    
-    light_sensor.set_als_contr(AlsGain::Gain4x, false, true).unwrap();
-    light_sensor
-        .set_ps_contr(true,true)
-        .unwrap();
-
-    
-    light_sensor.set_ps_meas_rate(PsMeasRate::_100ms).unwrap();
-    light_sensor
-        .set_als_meas_rate(AlsIntTime::_50ms, AlsMeasRate::_50ms)
-        .unwrap();
-    
-    light_sensor.set_ps_offset(0).unwrap();
-    light_sensor.set_ps_low_limit_raw(0).unwrap();
-    light_sensor.set_ps_high_limit_raw(1000).unwrap();
-    
-    let als_status = light_sensor.get_als_contr().unwrap();
-    let ps_status = light_sensor.get_ps_contr().unwrap();
-    tracing::info!("Manufactured by {manufacturer_id:x} : {part_id:x} status {als_status:x} {ps_status:x}" );
-    
-    let mut delay = Delay;
-    let mut display = st7735_lcd::ST7735::new(spi, dc, NotConnected, false, true, 162, 132);
-    display
-        .init(&mut delay)
-        .expect("Failed to initialize display");
-    display
-        .set_orientation(&Orientation::LandscapeSwapped)
-        .unwrap();
-    let color = Rgb565::BLUE;
-    display.clear(color).unwrap();
-    let color = Rgb565::RED;
-    display.clear(color).unwrap();
-
     let small_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
     let style = PrimitiveStyleBuilder::new()
         .stroke_color(Rgb565::BLUE)
@@ -171,7 +191,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         let h = 10;
         let x = i * w as i32;
         let y = i * h as i32;
-        Text::new("Hello PiWorld", Point::new(x, y), small_style)
+        Text::new("Hello ROBIN", Point::new(x, y), small_style)
             .draw(&mut canvas)
             .map_err(|_| "Can't draw")?;
     }
@@ -230,4 +250,73 @@ fn main() -> Result<(), Box<dyn Error>> {
             measurements.pressure / 100.0
         );
     }
+}
+
+fn init_pth_sensors(i2c: I2c) -> Result<BME280<I2c,Delay>, Box<dyn Error> > {
+    let mut bme280 = BME280::new_primary(i2c, Delay);
+    bme280.init().map_err(|e| format!("{e:?}"))?;
+    Ok(bme280)        
+}
+
+fn init_light_sensor(i2c : I2c) -> Result<Ltr559<I2c, ltr_559::ic::Ltr559>, Box<dyn Error> > {
+    let mut light_sensor = Ltr559::new_device(i2c, SlaveAddr::default());
+    light_sensor.set_als_contr(AlsGain::Gain4x, true, false).map_err(|e| format!("{e:?}"))?;
+    thread::sleep(Duration::from_millis(100));
+    light_sensor.reset_internal_driver_state();
+    thread::sleep(Duration::from_millis(100));
+    
+
+    let light_sensor_manufacturer_id = light_sensor.get_manufacturer_id().map_err(|e| format!("{e:?}"))?;
+    let light_sensor_part_id = light_sensor.get_part_id().map_err(|e| format!("{e:?}"))?;
+    
+    tracing::info!("LTR559 Manufactured by {light_sensor_manufacturer_id:x} : {light_sensor_part_id:x}");        
+    
+    light_sensor
+        .set_interrupt(InterruptPinPolarity::Low, InterruptMode::Both)
+        .map_err(|e| format!("{e:?}"))?;
+            
+    light_sensor.set_ps_led(LedPulse::Pulse30, LedDutyCycle::_100, LedCurrent::_50mA).map_err(|e| format!("{e:?}"))?;
+    light_sensor.set_ps_n_pulses(1).map_err(|e| format!("{e:?}"))?;
+    
+    light_sensor.set_als_contr(AlsGain::Gain4x, false, true).map_err(|e| format!("{e:?}"))?;
+    light_sensor
+        .set_ps_contr(true,true)
+        .map_err(|e| format!("{e:?}"))?;
+
+    
+    light_sensor.set_ps_meas_rate(PsMeasRate::_100ms).map_err(|e| format!("{e:?}"))?;
+    light_sensor
+        .set_als_meas_rate(AlsIntTime::_50ms, AlsMeasRate::_50ms)
+        .map_err(|e| format!("{e:?}"))?;
+    
+    light_sensor.set_ps_offset(0).map_err(|e| format!("{e:?}"))?;
+    light_sensor.set_ps_low_limit_raw(0).map_err(|e| format!("{e:?}"))?;
+    light_sensor.set_ps_high_limit_raw(1000).map_err(|e| format!("{e:?}"))?;
+    Ok(light_sensor)
+}
+
+fn init_display(gpio: Gpio) -> Result<st7735_lcd::ST7735<Spi,rppal::gpio::OutputPin , NotConnected>, Box<dyn Error> >{
+    let mut delay = Delay;
+    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss1, SPI_SPEED, SPIMode::Mode0)?;
+
+    let dc = gpio.get(9)?.into_output();
+    let mut display = st7735_lcd::ST7735::new(spi, dc, NotConnected, false, true, 162, 132);
+    display
+        .init(&mut delay).map_err(|_| "Can't initialize the display")?;
+    display
+        .set_orientation(&Orientation::LandscapeSwapped).map_err(|_| "Can't set the orientation")?;
+
+    let color = Rgb565::BLUE;
+    display.clear(color).map_err(|_| "Can't clear the display")?;
+    let color = Rgb565::RED;
+    display.clear(color).map_err(|_| "Can't clear the display")?;
+    Ok(display)
+}
+
+fn register_proximity_callback(gpio: Gpio) -> Result<(),Box<dyn Error>>{
+    let mut proximity = gpio.get(4)?.into_input_pullup();
+    proximity.set_async_interrupt(Trigger::FallingEdge, |c: Level| {
+        tracing::warn!("Got interrupt level {c:?}");
+    })?;
+    Ok(())
 }
